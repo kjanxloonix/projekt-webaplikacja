6eaa68de9f25469fd1180db1f7e711dabb972350
mskwyditd <slowik.tomasz@wp.pl>
2022-10-16 11:31:18 +0000

    added bootstrap

--- website/website/settings.py
+++ website/website/settings.py
@@ -16,6 +16,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Add bootstrap folder to STATIC_DIRS
+os.path.join(BASE_DIR, 'boot')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
