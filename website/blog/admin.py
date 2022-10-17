700741ece1e69d4061d93ea9deb607409dc5d4a7
kjanxloonix <kja@onet.eu>
2022-10-17 18:59:16 +0200

    Added comments

--- website/blog/admin.py
+++ website/blog/admin.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
 # Template from https://github.com/TheAbhijeet/Django_blog/blob/1/blog/admin.py
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'status','created_on')
-    list_filter = ("status",)
+    list_filter = ('status','created_on')
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
 
-admin.site.register(Post)
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'active')
+    list_filter = ('active', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
+
+admin.site.register(Post, PostAdmin)
