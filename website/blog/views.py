0232d4035a50d362c5b6e4650032847c18731a03
kjanxloonix <kja@onet.eu>
2022-10-18 19:24:15 +0200

    Added navigation buttons

--- website/blog/views.py
+++ website/blog/views.py
@@ -9,10 +9,7 @@ from .forms import CommentForm
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
-
-#class PostDetail(generic.DetailView):
-#    model = Post
-#    template_name = 'post_detail.html'
+    paginate_by = 4
 
 def post_detail(request, slug):
     template_name = "post_detail.html"
